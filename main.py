5df44fc30668b423430e3f076006eb55c37f2015
Isami <53663257+isamirivers@users.noreply.github.com>
2025-04-16 11:05:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -108,6 +108,9 @@ def login():
     login = data.get('login')
     password = data.get('password')
     user = users_collection.find_one({'login': login})
+    print(f"Password entered by user: {password}")
+    print(f"Hashed password entered by user: {generate_password_hash(password)}")
+    print(f"Hashed password in database: {user['password']}")
     if user and check_password_hash(user['password'], password):
         session['user_id'] = str(user['_id'])
         return 'OK'
